5f3a3c0d1b240b3b781345fbd5720acf7ed4c940
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-05-26 13:47:56 +0300

    v1.0.4

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Logstash = require('logstash-client');
 const Stringify = require('fast-safe-stringify');
 
-module.exports = () => {
+module.exports = (options) => {
+    if (!options) options = {};
     if (!process.env.LOGSTASH_HOST) throw new Error('Env var LOGSTASH_HOST is not defined');
     if (!process.env.LOGSTASH_PORT) throw new Error('Env var LOGSTASH_PORT is not defined');
     if (!process.env.TYPE_LOG) throw new Error('Env var TYPE_LOG is not defined');
@@ -14,21 +15,25 @@ module.exports = () => {
             return Stringify(message);
         }
     });
-    const send = (data1, data2) => {
-        if (typeof data1 !== 'Object') data1 = {kind: data1};
-        if (!data2) data2 = {};
-        console.log(Object.assign(data1, data2));
-        logstash.send(Object.assign(data1, data2));
-    };
     return {
         // 'fatal', 'error', 'warn', 'info', 'debug', 'trace'
-        info: send,
+        info: (data1, data2) => {
+            if (typeof data1 !== 'Object') data1 = {kind: data1};
+            if (!data2) data2 = {};
+            const data = Object.assign(data1, data2);
+            if (options.stdout) console.error(data);
+            data.type = 'pino';
+            logstash.send(data);
+        },
         error: (err) => {
+            if (options.stdout) console.error(err);
             if (typeof err === 'Object') {
+                err.type = 'pino';
                 err.kind = 'error';
                 logstash.send(err);
             } else {
                 logstash.send({
+                    type: 'pino',
                     kind: 'error',
                     err
                 });
